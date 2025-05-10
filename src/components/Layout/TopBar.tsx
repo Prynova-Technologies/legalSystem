3a84d1b19335ca854fddf501a88c79b0e0fea5b8
ilyaseen19 <alfuty09@gmail.com>
2025-05-10 09:42:44 -0400

    style(Layout): update logout button styling and user info structure
    Refactor the logout button to use an icon and adjust its positioning. Additionally, update the user info structure to access user data correctly. These changes improve the visual consistency and usability of the top bar.

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -29,15 +29,17 @@ const TopBar: React.FC = () => {
           <FaIcons.FaBell />
           <span className="notification-badge">3</span>
         </div>
-        <div className="btn btn-primary" onClick={() => _handleLogout()}>
-          logout
-        </div>
+        
         <div className="user-info">
           <FaIcons.FaUserCircle className="user-avatar" />
           <div className="user-details">
-            <span className="user-name">{user ? `${user.firstName} ${user.lastName}` : 'User'}</span>
+            <span className="user-name">{user ? `${user.data.firstName} ${user.data.lastName}` : 'User'}</span>
             <span className="user-role">{user?.role || 'Role'}</span>
           </div>
+        </div>
+
+        <div className="notification-icon" onClick={() => _handleLogout()}>
+          <FaIcons.FaUserLock className="log-out" />
         </div>
       </div>
     </div>
